84dbfb7180de01428c1cf615487e4b1721fa7fff
carl-josiah <carl_josiah@yahoo.com>
2025-12-31 12:46:23 +0400

    fix: change new keyword to new_node

--- srcs/list/ft_lstadd_back.c
+++ srcs/list/ft_lstadd_back.c
@@ -6,19 +6,19 @@
 /*   By: ccastro <ccastro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 10:57:51 by ccastro           #+#    #+#             */
-/*   Updated: 2025/08/28 18:18:22 by ccastro          ###   ########.fr       */
+/*   Updated: 2025/12/31 12:45:38 by ccastro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incs/libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new_node)
 {
 	t_list	*curr;
 
 	curr = ft_lstlast(*lst);
 	if (curr == NULL)
-		*lst = new;
+		*lst = new_node;
 	else
-		curr->next = new;
+		curr->next = new_node;
 }
--- srcs/list/ft_lstadd_front.c
+++ srcs/list/ft_lstadd_front.c
@@ -6,16 +6,16 @@
 /*   By: ccastro <ccastro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 18:44:32 by ccastro           #+#    #+#             */
-/*   Updated: 2025/08/28 18:18:24 by ccastro          ###   ########.fr       */
+/*   Updated: 2025/12/31 12:46:04 by ccastro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incs/libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new_node)
 {
-	if (!new)
+	if (!new_node)
 		return ;
-	new->next = *lst;
-	*lst = new;
+	new_node->next = *lst;
+	*lst = new_node;
 }
